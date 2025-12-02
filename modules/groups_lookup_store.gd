c816fcef01e38dd6242f3176d962293ba4ffd414
GekkotaDev <114091010+GekkotaDev@users.noreply.github.com>
2025-12-02 23:07:19 +0800

    work

---
+++ modules/groups_lookup_store.gd
@@ -0,0 +1,5 @@
+class_name GroupsLookupStore
+
+class Entities:
+	const PLAYERS = "entities/players"
+	const POWERUP = "entities/powerup"
---
+++ nodes/powerup/powerup_container/powerup_container.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@export var collision_area: Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    var player: Node3D = null
+    var bodies := collision_area.get_overlapping_bodies()
+
+    for body in bodies:
+        if not body.is_in_group(GroupsLookupStore.Entities.PLAYERS):
+            continue
+        player = body
+        break
